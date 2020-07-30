551f0d7055d80fdcbefd281904c9885e8819c1c9
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-30 18:10:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length >= 1 
-    puts "Currently serving #{katz_deli[0]}"
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
     else katz_deli.empty?
     puts "There is nobody waiting to be served!"
